3c60eb80f02959db456750c0f65b43c9ca6c334c
Rutger Rauws <rutgerrauws@gmail.com>
2020-06-19 11:20:09 +0200

    Added basic render window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include <vtkPolyDataReader.h>
 #include <vtkSmartPointer.h>
 #include <vtkGenericDataObjectReader.h>
@@ -8,6 +9,8 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkConeSource.h>
+#include <vtkPolyDataMapper.h>
 
 vtkSmartPointer<vtkPolyData> readPolyData(const std::string& inputFileName);
 
@@ -15,9 +18,9 @@ int main()
 {
     std::string inputFileName = "./data/FiberBundle_1_Output Volume-label.vtk";
     std::cout << "Application started." << std::endl;
-    
+
     vtkSmartPointer<vtkPolyData> output;
-    
+
     try {
         output = readPolyData(inputFileName);
         std::cout << "output has " << output->GetNumberOfLines() << " lines." << std::endl;
@@ -26,6 +29,29 @@ int main()
         std::cout << e.what() << std::endl;
         return EXIT_FAILURE;
     }
+
+    vtkSmartPointer<vtkConeSource> cone = vtkSmartPointer<vtkConeSource>::New();
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    //mapper->SetInputConnection(cone->GetOutputPort());
+    mapper->SetInputData(output);
+    
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+    actor->SetMapper(mapper);
+    
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor(actor);
+    renderer->SetBackground(0, 0, 0);
+    
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->SetSize(800, 600);
+    renderWindow->AddRenderer(renderer);
+    
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+    
+    renderer->ResetCamera();
+    renderWindow->Render();
+    renderWindowInteractor->Start();
     
     return EXIT_SUCCESS;
 }
