5e81a95d41be49557f0b27ed92f2b8ece9051d8f
Rutger Rauws <rutgerrauws@gmail.com>
2020-12-11 21:29:09 +0100

    Restructured data folder

--- main.h
+++ main.h
@@ -13,40 +13,29 @@
 /*
  * Temporary hardcoded test input files
  */
-//const std::string INPUT_FILE_NAME = "./data/corpus-callosum.vtk";
-//const std::string INPUT_FILE_NAME = "./data/fiber-samples-without-outliers.vtk";
-//const std::string INPUT_FILE_NAME = "./data/fiber-samples-with-outliers.vtk";
-//const std::string INPUT_FILE_NAME = "./data/cst-1.vtk";
-const std::string INPUT_FILE_NAME = "./data/cst-20.vtk";
-//const std::string INPUT_FILE_NAME = "./data/slf-1.vtk";
-//const std::string INPUT_FILE_NAME = "./data/slf-20.vtk";
-//const std::string INPUT_FILE_NAME = "./data/cc-5.vtk";
-//const std::string INPUT_FILE_NAME = "./data/for-each-seedpoint/1.vtk";
-
-//const std::vector<std::string> INPUT_FILE_NAMES = {
-//    "./data/cc-6/1.vtk",
-//    "./data/cc-6/2.vtk",
-//    "./data/cc-6/3.vtk",
-//    "./data/cc-6/4.vtk",
-//    "./data/cc-6/5.vtk",
-//    "./data/cc-6/6.vtk"
+//const std::vector<std::string> FIBER_FILE_NAMES = {
+//    "./data/fibers/cc/cc-6/1.vtk",
+//    "./data/fibers/cc/cc-6/2.vtk",
+//    "./data/fibers/cc/cc-6/3.vtk",
+//    "./data/fibers/cc/cc-6/4.vtk",
+//    "./data/fibers/cc/cc-6/5.vtk",
+//    "./data/fibers/cc/cc-6/6.vtk"
 //};
 
 const std::vector<std::string> FIBER_FILE_NAMES = {
-        "./data/cst-10/1.vtk",
-        "./data/cst-10/2.vtk",
-        "./data/cst-10/3.vtk",
-        "./data/cst-10/4.vtk",
-        "./data/cst-10/5.vtk",
-        "./data/cst-10/6.vtk",
-        "./data/cst-10/7.vtk",
-        "./data/cst-10/8.vtk",
-        "./data/cst-10/9.vtk",
-        "./data/cst-10/10.vtk",
+        "./data/fibers/cst/cst-10/1.vtk",
+        "./data/fibers/cst/cst-10/2.vtk",
+        "./data/fibers/cst/cst-10/3.vtk",
+        "./data/fibers/cst/cst-10/4.vtk",
+        "./data/fibers/cst/cst-10/5.vtk",
+        "./data/fibers/cst/cst-10/6.vtk",
+        "./data/fibers/cst/cst-10/7.vtk",
+        "./data/fibers/cst/cst-10/8.vtk",
+        "./data/fibers/cst/cst-10/9.vtk",
+        "./data/fibers/cst/cst-10/10.vtk",
 };
 
-
-const std::string DWI_PATH = "/home/rutger/Desktop/Graduation/standalone/progressive-fiber-uncertainty-viz/data/test-data-dwi-volume.nhdr";
+const std::string DWI_PATH = "./data/mri/test-data-dwi-volume.nhdr";
 
 #define DEBUG
 
